ef148ef4aa35b7b003e9f7be98c58af659dc8e32
Владислав Кузнецов <v.kuznetsov@ideco.ru>
2024-12-08 20:48:03 +0500

    Подобрал кэффы стратегии

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Для бектеста
 
 start_cash = 1000000
-data__path = 'data/test_zachet_data.csv'
+data__path = 'data/data.csv'
 start_date = '2021-01-11'
 end_date = '2022-12-01'
 format_date = '%Y-%m-%d'
@@ -9,11 +9,11 @@ name = "Кузнецов Владислав МЕН-320203 3 курс"
 
 # Для стратегий
 short_period = 3
-long_period = 14
-rsi_period = 8
-short_sma_period = 8
+long_period = 20
+
+short_sma_period = 7
 long_sma_period = 20
 
-risk = 0.001
-stop_loss = 171
+risk = 0.09
+stop_loss = 168
 upper_stop_loss = 150
